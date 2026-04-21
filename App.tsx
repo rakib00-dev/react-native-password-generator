6271cd0125f10f6f7c22562f6cf0ddd25615d346
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-21 07:24:10 +0600

    added passwordSchema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React, { JSX } from 'react';
+import * as Yup from 'yup';
+
+const passwordSchema = Yup.object({
+  passworkLength: Yup.number()
+    .min(4, 'Should be min of 4 charactor')
+    .max(16, 'Should be max of 16 charactor')
+    .required.apply('Password Length is Required'),
+});
 
 export default function App(): JSX.Element {
   return (
