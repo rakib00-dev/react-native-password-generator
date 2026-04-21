50d199790b3099af54114b4bb582cb1b383529e4
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-21 07:44:37 +0600

    Updated the app.tsx

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { JSX } from 'react';
+import React from 'react';
 import * as Yup from 'yup';
 
-const passwordSchema = Yup.object({
+const passwordSchema = Yup.object().shape({
   passworkLength: Yup.number()
     .min(4, 'Should be min of 4 charactor')
     .max(16, 'Should be max of 16 charactor')
-    .required.apply('Password Length is Required'),
+    .required('Password Length is Required'),
 });
 
-export default function App(): JSX.Element {
+export default function App() {
   return (
     <View>
       <Text>App</Text>
